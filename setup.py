5eef7fa2e01b2144c35fda0e57150738b0cdf650
Assaf Gordon <assafgordon@gmail.com>
2022-03-01 10:12:43 -0700

    Add stub python-package files

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+RenertPY Python Package
+Copyright (C) 2022 Assaf Gordon (assafgordon@gmail.com)
+License: BSD (See LICENSE file)
+"""
+
+from setuptools import setup
+from detect_version import detect_version
+
+setup(
+    name = "renertpy",
+    version = detect_version("renertpy"),
+    author = "Assaf Gordon",
+    author_email = "AssafGordon@gmail.com",
+
+    description = "Collection of functions used to teach Python at Renert School",
+    long_description="",
+
+    license = "BSD",
+    keywords = "jupyter",
+    url = "https://github.com/agordon/renertpy",
+    packages=['renertpy'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+    ],
+    #test_suite = 'tests',
+)
