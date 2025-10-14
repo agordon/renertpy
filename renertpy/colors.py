396217aea7133feaac870addd9a782885cd07da7
Assaf Gordon <assafgordon@gmail.com>
2025-10-13 22:53:25 -0600

    colors: hexcolor_to_rgb: new function

--- renertpy/colors.py
+++ renertpy/colors.py
@@ -31,3 +31,34 @@ def hsv_to_rgb(h,s,v):
     r,g,b = colorsys.hsv_to_rgb(h/360,s,v)
 
     return [int(r*255), int(g*255), int(b*255)]
+
+
+def hexcolor_to_rgb(cstr):
+    # Python's Turtle has an undocumented function for this
+    # ( TBuffer._color(str,cstr) ) which is nice an efficient,
+    # but not user-friendly when given invalid input.
+    # Since this module is meant for newbies, be less efficient and
+    # more verbose with invalid input.
+    if not cstr.startswith("#"):
+        raise ValueError(f"invalid hex-color string '{cstr}' - must start with '#'")
+
+    # Check if all the characeters (after '#') are hex-digits.
+    # Here we don't yet care about the actual length - any length of valid hex-digits will work.
+    # This code is less intuitive than standard check (e.g. regex or loop) but is more efficient
+    # (as this is expected to be called once per pixel)
+    try:
+        dummy = int(cstr[1:], 16)
+    except ValueError:
+        raise ValueError(f"invalid hex-color string '{cstr}' - must start with '#' followed by 0-9,A-F characters")
+
+    if len(cstr)==7:
+        # Copied from turtle's _color()
+        cl = [int(cstr[i:i+2], 16) for i in (1, 3, 5)]
+    elif len(cstr)==4:
+        # Copied from turtle's _color()
+        cl = [16*int(h, 16) for h in cstr[1:]]
+    else:
+        raise ValueError(f"invalid hex-color string '{cstr}' - be '#' followed by 3 or 6 characters")
+
+    return tuple(cl)
+
